2de004d4eaf09f3b02dde3b7041ce6d693d0c25c
Cyan_Heron <templar.of.silence@gmail.com>
2021-11-25 11:08:18 +0100

    ReadMe

---
+++ experiments/experiments/Test8.py
@@ -0,0 +1,13 @@
+import argparse
+from experiments.experiments.PubIntegBackground import PubIntegBackground
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--intensity", type=float, default=5.0, help="...")
+    parser.add_argument("--repetitions", type=int, default=6, help="...")
+    parser.add_argument("--integr", default='11', help="...")
+    args, _ = parser.parse_known_args()
+
+    for _ in range(args.repetitions):
+        PubIntegBackground(correlation=False, listing=True, pub='None', integ_mode=args.integr, intensity=args.intensity)
+
